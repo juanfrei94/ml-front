06f2405d96392ccd228971af14b1a3c6c0f9e908
Juan Manuel Bottarelli Frei <juanmanuelbottarellifrei@Juans-MBP.fibertel.com.ar>
2024-07-11 23:00:43 -0300

    primer commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>
+      Hello world
+    </h1>
+  </React.StrictMode>,
+)
